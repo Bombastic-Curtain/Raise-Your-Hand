83cab190dd4e0cd253b5b02d3c079ba6282ae117
Kenny Langston <kenny.langston@gmail.com>
2015-05-21 16:28:01 -0700

    Add nav states to routing, create navbar

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,8 @@
+var defaultNav = { templateUrl: 'navbar.html',
+                   controller: function() {},
+                   controllerAs: 'navBar'};
+
+
   angular.module('queup', [
   'queup.factory',
   'queup.auth',
@@ -15,53 +20,103 @@
   $stateProvider
     .state('signup', {
       url: '/signup',
-      templateUrl: 'app/auth/signup.html',
-      controller: 'AuthController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/auth/signup.html',
+          controller: 'AuthController'}
+      }
+      
     })
     .state('signin', {
       url: '/signin',
-      templateUrl: 'app/auth/signin.html',
-      controller: 'AuthController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/auth/signin.html',
+          controller: 'AuthController'
+        }
+      }
     })
     .state('before_session', {
       url: '/before_session',
-      templateUrl: 'app/before_session/before_session.html',
-      controller: 'Before_sessionController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/before_session/before_session.html',
+          controller: 'Before_sessionController'
+        }
+      },
     })
     .state('before_session.class_info', {
       url: '/class_info',
-      templateUrl: 'app/before_session/class_info.html',
-      controller: 'Class_infoController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/before_session/class_info.html',
+          controller: 'Class_infoController'
+        }
+      },
     })
     .state('before_session.class_list', {
       url: '/class_list',
-      templateUrl: 'app/before_session/class_list.html',
-      controller: 'Class_listController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/before_session/class_list.html',
+          controller: 'Class_listController'
+        }
+      }, 
     })
     .state('before_session.new_class', {
       url: '/new_class',
-      templateUrl: 'app/before_session/new_class.html',
-      controller: 'New_classController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/before_session/new_class.html',
+          controller: 'New_classController'
+        }
+      },
     })
     .state('in_session', {
       url: '/in_session',
-      templateUrl: 'app/in_session/in_session.html',
-      controller: 'In_sessionController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/in_session/in_session.html',
+          controller: 'In_sessionController'
+        }
+      },
     })
     .state('in_session.queue_list', {
       url: '/queue_list',
-      templateUrl: 'app/in_session/queue_list.html',
-      controller: 'Queue_listController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/in_session/queue_list.html',
+          controller: 'Queue_listController'
+        }
+      },
     })
     .state('in_session.student_list', {
       url: '/student_list',
-      templateUrl: 'app/in_session/student_list.html',
-      controller: 'Student_listController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/in_session/student_list.html',
+          controller: 'Student_listController'
+        }
+      },
     })
     .state('in_session.class_settings', {
       url: '/class_settings',
-      templateUrl: 'app/in_session/class_settings.html',
-      controller: 'Class_settingsController'
+      views: {
+        'nav': defaultNav,
+        'body': {
+          templateUrl: 'app/in_session/class_settings.html',
+          controller: 'Class_settingsController'
+        }
+      },
     })
 
   $urlRouterProvider.otherwise('/signin');
