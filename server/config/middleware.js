5f3b34c5bb1cb2ffca0aee695b038a6aa6065a17
Rocky Tang <rocky.tang168@gmail.com>
2015-05-25 15:25:21 -0700

    add CORS headers

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -18,6 +18,15 @@ module.exports = function (app, express) {
   app.use(passport.initialize());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+
+  //add CORS HEADERS
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
   /*
     THIS NEED TO BE CHANGED TO /../../client TO SERVE UP THE ANGULAR FRONTEND
   */
