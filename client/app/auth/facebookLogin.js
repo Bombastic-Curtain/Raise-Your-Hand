497a572c4ce47bae3902cf4b3534d1bccaab986f
Rocky Tang <rocky.tang168@gmail.com>
2015-05-26 19:58:29 -0700

    update css styling for before session and in session

--- client/app/auth/facebookLogin.js
+++ client/app/auth/facebookLogin.js
@@ -35,7 +35,7 @@
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : '936996249655076',
+    appId      : '718396624937121',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
@@ -50,13 +50,13 @@
   };
 
   // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src="https://connect.facebook.net/en_US/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+ (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
