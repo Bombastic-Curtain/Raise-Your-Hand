8c076d083aa00fce72333524e5b540619a751c35
John Pizzo <jjpizzo4@gmail.com>
2015-05-27 19:23:48 -0700

    Change path to deployed server

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,7 +2,7 @@ angular.module('queup.factory', [])
 
 .factory('queupFactory', function($http, $rootScope){
 
-  $rootScope.serverURL = 'http://localhost:8000' ; // 'http://queup.io'
+  $rootScope.serverURL = 'http://queup.io';//'http://localhost:8000' ;
 
   var addNewClass = function(newClassName){
     console.log(newClassName)
