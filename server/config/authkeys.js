f5ae45e6c47f7979a5905664c22467ac233f7905
John Pizzo <jjpizzo4@gmail.com>
2015-05-27 19:07:24 -0700

    New facebook api key

--- server/config/authkeys.js
+++ server/config/authkeys.js
@@ -1,7 +1,7 @@
 // =====
 module.exports = {
 	'facebookAuth' : {
-		'clientID' : '718396624937121',
-		'clientSecret' : 'f5e9acd0b2cbfc6e46066cf185758ef8'
+		'clientID' : '1425134197808858',
+		'clientSecret' : '02c6f7d9cd561b61d7c68a4fe71bdb1f'
 	}
 }
